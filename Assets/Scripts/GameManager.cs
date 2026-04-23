27d3702afc35e881f2dc240f28b084b71222eedc
Milbakp <luqmanirfan618@gmail.com>
2026-04-23 19:17:34 +0800

    First commit on alienware

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello world! First time using Unity on this computer. I am bacn in C# and... this feels like home!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
